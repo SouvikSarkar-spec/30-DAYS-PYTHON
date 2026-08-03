373dd20410b559b07887641417f903d68808dd0c
SouvikSarkar-spec <souviksarkarraj2006@gmail.com>
2026-08-03 15:47:21 +0530

    Second day

---
+++ day_2/variables.py
@@ -0,0 +1,54 @@
+#Day 2: 30 Days of Python Programming
+
+#excersice 1:
+first_name="souvik"
+last_name="sarkar"
+full_name="Souvik sarkar"
+country="India"
+city="paris"
+age=20
+year=2026
+is_married=False
+is_true=True
+is_light_on=True
+height,animal,weight=5.9,"dog",60
+
+
+#excersice 2:
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(height))
+print(type(animal))
+print(type(weight))
+print(len(first_name))
+print(len(last_name))
+print("First name is longer than last name:", len(first_name) > len(last_name))
+num_one=5
+num_two=4
+total=num_one+num_two
+diff=num_two-num_one
+product=num_one*num_two
+division=num_one/num_two
+modulus=num_two%num_one
+exp=num_one**num_two
+floor_division=num_one//num_two
+radius=30
+area_of_circle=3.14*radius**2 
+circumference_of_circle=2*3.14*radius
+print("The area of a circle is", area_of_circle)
+print("The circumference of a circle is", circumference_of_circle)
+input_radius=int(input("Enter the radius of a circle:"))
+area_of_circle_input=3.14*input_radius**2
+print("The area of a circle with radius", input_radius, "is", area_of_circle_input)
+input_first_name=input("Enter your first name:")
+input_last_name=input("Enter your last name:") 
+input_country=input("Enter your country:")
+input_age=int(input("Enter your age:"))
