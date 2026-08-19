39acfbe0101a20ba9f411779a00eb2ede72bd4f9
SouvikSarkar-spec <souviksarkarraj2006@gmail.com>
2026-08-19 23:04:13 +0530

    complete part of 13day

--- day_13/excersice.py
+++ day_13/excersice.py
@@ -11,3 +11,18 @@ print(lst_2)
 countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
 lst_3=[list(item) for sublist in countries for item in sublist]
 print(lst_3)
+lst_4=[(country.upper(),country[:3].upper(),capital.upper()) for [(country,capital)] in countries]
+print(lst_4)
+lst_5=[{'country':a,'city':b} for [(a,b)] in countries]
+print(lst_5)
+names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+lst_6=[name+' '+surname for [(name,surname)] in names]
+print(lst_6)
+
+def ans(type):
+     type=type.capitalize()
+     if type=="Slope":
+          return lambda x,y,x_1,y_1:(y_1-y)/(x_1-x)
+     return lambda x,y,x_1,y_1:y-((y_1-y)/(x_1-x))*x
+print(ans("slope")(1,2,5,6))
+          
