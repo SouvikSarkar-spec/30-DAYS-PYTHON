80d1a45c3f21d74068338e5cfa82456d9d673c9a
SouvikSarkar-spec <souviksarkarraj2006@gmail.com>
2026-08-19 23:53:09 +0530

    complete

--- day_11/excersice.py
+++ day_11/excersice.py
@@ -1,228 +1,228 @@
-# def add_two_function(a,b):
-#     return a+b
-# print(add_two_function(1,2))
-# def area_of_circle(r):
-#     return 3.14*r*r
-# print(area_of_circle(3))
+def add_two_function(a,b):
+    return a+b
+print(add_two_function(1,2))
+def area_of_circle(r):
+    return 3.14*r*r
+print(area_of_circle(3))
 
-# def add_all_nums(*args):
+def add_all_nums(*args):
     
-#     for types in args:
-#         if type(types) != int and type(types) != float:
-#             x = type(types)
-#             return(f"it is a {x} you dumbass")
-
-    
-#     print("ok to proceed")
-#     total = 0
-#     for arg in args:
-#         total += arg
-
-#     return total  
-
-# print(add_all_nums(1, 2, 5, 2))
-# print(add_all_nums(5, 6, "lol", 2, 5, 2))
-
-# def convert_c_to_f(a):
-#     f=0
-#     f=((a*9)/5)+32
-#     return f
-
-# print(convert_c_to_f(35))
-# def check_season(month):
-#     month = month.capitalize()
-#     if month in ["December", "January", "February"]:
-#         return "Winter"
-#     elif month in ["March", "April", "May"]:
-#         return "Spring"
-#     elif month in ["June", "July", "August"]:
-#         return "Summer"
-#     elif month in ["September", "October", "November"]:
-#         return "Autumn"
-#     else:
-#         return "Invalid month"
+    for types in args:
+        if type(types) != int and type(types) != float:
+            x = type(types)
+            return(f"it is a {x} you dumbass")
 
     
-# print(check_season("march"))
+    print("ok to proceed")
+    total = 0
+    for arg in args:
+        total += arg
+
+    return total  
+
+print(add_all_nums(1, 2, 5, 2))
+print(add_all_nums(5, 6, "lol", 2, 5, 2))
+
+def convert_c_to_f(a):
+    f=0
+    f=((a*9)/5)+32
+    return f
+
+print(convert_c_to_f(35))
+def check_season(month):
+    month = month.capitalize()
+    if month in ["December", "January", "February"]:
+        return "Winter"
+    elif month in ["March", "April", "May"]:
+        return "Spring"
+    elif month in ["June", "July", "August"]:
+        return "Summer"
+    elif month in ["September", "October", "November"]:
+        return "Autumn"
+    else:
+        return "Invalid month"
+
+    
+print(check_season("march"))
 
 
-# def calculate_slope(x,y):
-#     if x==0:
-#         return("it is undefined")     
-#     return y/x
+def calculate_slope(x,y):
+    if x==0:
+        return("it is undefined")     
+    return y/x
 
-# print(calculate_slope(7,4))
+print(calculate_slope(7,4))
 
-# def solve_quadratic_eqn(a,b,c):
-#     root1=0
-#     root2=0
-#     root1=(-b-(b**2-4*a*c)**0.5)/(2*a)
-#     root2=(-b+(b**2-4*a*c)**0.5)/(2*a)
-#     return {root1,root2}
-# print(solve_quadratic_eqn(1,5,6))
+def solve_quadratic_eqn(a,b,c):
+    root1=0
+    root2=0
+    root1=(-b-(b**2-4*a*c)**0.5)/(2*a)
+    root2=(-b+(b**2-4*a*c)**0.5)/(2*a)
+    return {root1,root2}
+print(solve_quadratic_eqn(1,5,6))
 
-# def reverse_list(laist):
+def reverse_list(laist):
+    laist.sort(reverse=True)
+    return laist
+print(reverse_list([1, 2, 3, 4, 5]))
+# [5, 4, 3, 2, 1]
+print(reverse_list(["A", "B", "C"])) 
+# ["C", "B", "A"]
+
+def add_item(laist,item):
+    laist.append(item)
+    return laist
+
+food_stuff = ['Potato', 'Tomato', 'Mango', 'Milk'];
+print(add_item(food_stuff, 'Meat'))     # ['Potato', 'Tomato', 'Mango', 'Milk','Meat'];
+numbers = [2, 3, 7, 9];
+print(add_item(numbers, 5))      # [2, 3, 7, 9, 5]
+
+def sum_of_numbers(a):
+    total=0
+    for num in range(a+1):
+        total+=num
+    return total    
+print(sum_of_numbers(5))  # 15
+print(sum_of_numbers(10)) # 55
+print(sum_of_numbers(100)) # 5050
+
+
+def evens_and_odds(a):
+    even_list=[]
+    odd_list=[]
+    for num in range(a+1):
+        if num%2==0:
+            even_list.append(num)
+        else:
+         odd_list.append(num)
+    return f"number of even {len(even_list)}\nnumber of odd {len(odd_list)}"      
+
+print(evens_and_odds(100))
+    # The number of odds are 50.
+    # The number of evens are 51.
+
+def factorial(a):
+   total=1
+   for num in range(1,a+1,1):
+      total=total*num
+   return total  
+print(factorial(5))
+
+def is_empty(a):
+    if a==[]:
+      return f"{a} is empty"
+    else:
+       return f"{a} is not empty"
+
+print(is_empty([1,23,4]))
+
+def calculate_mean(a):
+    total=0
+    for num in a:
+        total+=num
+    return total/len(a)
+print(calculate_mean([1,4,5,6,7,8,9]))
+
+# def calculate_mode(a):
+#     b=set(a)
+#     laist=[]
+#     for arg in b:
+#         x=a.count(arg)
+#         laist.append((x,arg))
 #     laist.sort(reverse=True)
-#     return laist
-# print(reverse_list([1, 2, 3, 4, 5]))
-# # [5, 4, 3, 2, 1]
-# print(reverse_list(["A", "B", "C"])) 
-# # ["C", "B", "A"]
-
-# def add_item(laist,item):
-#     laist.append(item)
-#     return laist
-
-# food_stuff = ['Potato', 'Tomato', 'Mango', 'Milk'];
-# print(add_item(food_stuff, 'Meat'))     # ['Potato', 'Tomato', 'Mango', 'Milk','Meat'];
-# numbers = [2, 3, 7, 9];
-# print(add_item(numbers, 5))      # [2, 3, 7, 9, 5]
-
-# def sum_of_numbers(a):
-#     total=0
-#     for num in range(a+1):
-#         total+=num
-#     return total    
-# print(sum_of_numbers(5))  # 15
-# print(sum_of_numbers(10)) # 55
-# print(sum_of_numbers(100)) # 5050
-
-
-# def evens_and_odds(a):
-#     even_list=[]
-#     odd_list=[]
-#     for num in range(a+1):
-#         if num%2==0:
-#             even_list.append(num)
-#         else:
-#          odd_list.append(num)
-#     return f"number of even {len(even_list)}\nnumber of odd {len(odd_list)}"      
-
-# print(evens_and_odds(100))
-#     # The number of odds are 50.
-#     # The number of evens are 51.
-
-# def factorial(a):
-#    total=1
-#    for num in range(1,a+1,1):
-#       total=total*num
-#    return total  
-# print(factorial(5))
-
-# def is_empty(a):
-#     if a==[]:
-#       return f"{a} is empty"
-#     else:
-#        return f"{a} is not empty"
-
-# print(is_empty([1,23,4]))
-
-# def calculate_mean(a):
-#     total=0
-#     for num in a:
-#         total+=num
-#     return total/len(a)
-# print(calculate_mean([1,4,5,6,7,8,9]))
-
-# # def calculate_mode(a):
-# #     b=set(a)
-# #     laist=[]
-# #     for arg in b:
-# #         x=a.count(arg)
-# #         laist.append((x,arg))
-# #     laist.sort(reverse=True)
-# #     y=laist[0]
-# #     return f"the mode is {y[1]}"
+#     y=laist[0]
+#     return f"the mode is {y[1]}"
  
-# # print(calculate_mode([1,4,6,5,6,7,8,9]))
+# print(calculate_mode([1,4,6,5,6,7,8,9]))
     
-# def calculate_median(a):
-#     a.sort()
-#     if len(a)%2==0:
-#         mid1=a[len(a)//2]
-#         mid2=a[(len(a)-1)//2]
-#         return f"the median of {a} is {(mid1+mid2)/2}"
-#     else:
-#         return f"the median of {a} is {a[len(a)//2]}"
-# print(calculate_median([1,4,6,5,6,7,8,9]))
+def calculate_median(a):
+    a.sort()
+    if len(a)%2==0:
+        mid1=a[len(a)//2]
+        mid2=a[(len(a)-1)//2]
+        return f"the median of {a} is {(mid1+mid2)/2}"
+    else:
+        return f"the median of {a} is {a[len(a)//2]}"
+print(calculate_median([1,4,6,5,6,7,8,9]))
 
-# def calculate_range(a):
-#     a.sort()
-#     small=a[0]
-#     big=a[-1]
-#     range_val=big-small
-#     return f"range is {range_val}"
-# print(calculate_range([1,4,6,5,6,7,8,9]))
+def calculate_range(a):
+    a.sort()
+    small=a[0]
+    big=a[-1]
+    range_val=big-small
+    return f"range is {range_val}"
+print(calculate_range([1,4,6,5,6,7,8,9]))
 
-# def cal_variance(a):
-#     y=0
-#     mean=calculate_mean(a)
-#     for arg in a:
-#         y+=(arg-mean)**2
-#     variance=y/len(a)
-#     return variance 
+def cal_variance(a):
+    y=0
+    mean=calculate_mean(a)
+    for arg in a:
+        y+=(arg-mean)**2
+    variance=y/len(a)
+    return variance 
 
-# print(cal_variance([1,4,6,5,6,7,8,9]))
-# def cal_std(a):
-#     return f"the std is {cal_variance(a)**0.5}" 
-# print(cal_std([1,4,6,5,6,7,8,9]))
-
-
-# def greet(name="Guest"):
-#     return f"\"hello,{name}!\""
+print(cal_variance([1,4,6,5,6,7,8,9]))
+def cal_std(a):
+    return f"the std is {cal_variance(a)**0.5}" 
+print(cal_std([1,4,6,5,6,7,8,9]))
 
 
-# print(greet())
-#     # "Hello, Guest!
-# print(greet("Alice"))
-#     # "Hello, Alice!"
-# def show_args(**args):
-#     formatted_items=[]
-#     for key,value in args.items():
-#         text=f"{key}:{value}"
-#         formatted_items.append(text)
-#     result = ", ".join(formatted_items)
-#     return f"Received: {result}"
+def greet(name="Guest"):
+    return f"\"hello,{name}!\""
 
-# print(show_args(name="Alice", age=30, city="New York"))
-# # Received: name: Alice, age: 30, city: New York
-# print(show_args(name="Bob", pet="Fluffy, the bunny"))
-# # Received: name: Bob, pet: Fluffy, the bunny
 
-# def is_prime(a):
-#     if a<=1:
-#         return f"it is not prime"
+print(greet())
+    # "Hello, Guest!
+print(greet("Alice"))
+    # "Hello, Alice!"
+def show_args(**args):
+    formatted_items=[]
+    for key,value in args.items():
+        text=f"{key}:{value}"
+        formatted_items.append(text)
+    result = ", ".join(formatted_items)
+    return f"Received: {result}"
 
-#     for i in range(2,int(a**0.2)+1):
-#         if a%i==0:
-#             return f"nah dumb"
+print(show_args(name="Alice", age=30, city="New York"))
+# Received: name: Alice, age: 30, city: New York
+print(show_args(name="Bob", pet="Fluffy, the bunny"))
+# Received: name: Bob, pet: Fluffy, the bunny
+
+def is_prime(a):
+    if a<=1:
+        return f"it is not prime"
+
+    for i in range(2,int(a**0.2)+1):
+        if a%i==0:
+            return f"nah dumb"
         
-#     return True 
-# print(is_prime(11))
+    return True 
+print(is_prime(11))
 
-# def all_unique(lst):
-#     for item in lst:
-#        if lst.count(item) > 1:
-#         return False  
-#     return True
-# print(all_unique([1,5,4,2,6,4,8,5,1,5,8,2]))
+def all_unique(lst):
+    for item in lst:
+       if lst.count(item) > 1:
+        return False  
+    return True
+print(all_unique([1,5,4,2,6,4,8,5,1,5,8,2]))
 
-# def same_data_type(a):
-#    x=type(a[0])
-#    for y in a:
-#       if type(y)!=x:
-#          return f"not same type"
+def same_data_type(a):
+   x=type(a[0])
+   for y in a:
+      if type(y)!=x:
+         return f"not same type"
     
-#    return f"same type" 
-# print(same_data_type([1,5,4,True,6,4,8,5,1,5,8,2]))
-# def valid_python(x):
-#     valid_types = (bool, int, float, str, list)
+   return f"same type" 
+print(same_data_type([1,5,4,True,6,4,8,5,1,5,8,2]))
+def valid_python(x):
+    valid_types = (bool, int, float, str, list)
 
-#     if type(x) in valid_types:
-#         return "valid datatype"
+    if type(x) in valid_types:
+        return "valid datatype"
 
-#     return "invalid datatype"
-# print(valid_python(1))
+    return "invalid datatype"
+print(valid_python(1))
 
 data=[
     {
@@ -2844,3 +2844,19 @@ data=[
         "currency": "Botswana pula"
     }
 ]
+def most_spoken_languages(limit):
+    a=[a for country in data for a in country['languages']]
+    b=[(a.count(lang),lang) for lang in set(a)]
+    b.sort(reverse=True)
+    b=b[:limit]
+    return b
+
+
+print(most_spoken_languages(10))
+
+def most_populated_country(limit):
+    b=[(b['population'],b['name']) for b in data]
+    b.sort(reverse=True)
+    
+    return b[:limit]
+print(most_populated_country(10))
